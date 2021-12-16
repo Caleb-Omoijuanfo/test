7733ad2d9c28adcc2187bbfe7d1fded20741ff0e
Caleb Omoijuanfo <48349129+Caleb-Omoijuanfo@users.noreply.github.com>
2021-12-16 18:59:54 +0100

    Begin refactor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import { Navigation } from './components/Navigation';
 import './App.css';
 
-class App extends React.Component {
-  render(){
-    return(
-      <>
-        <Navigation />
-      </>
-    );
-  }
+const App = () => {
+  return <Navigation />
 }
 
 export default App;
